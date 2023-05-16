647352f2c97cbb8f66cf9038f93a100252986f47
lazyhysong <37135523+eee0930@users.noreply.github.com>
2023-05-16 16:27:11 +0900

    feat: Song Model 수정

--- src/music/models/Song.js
+++ src/music/models/Song.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-const songSchema = new mongoose.Schema({});
+const songSchema = new mongoose.Schema({
+
+  
+});
 
 const Song = mongoose.model("Song", songSchema);
 
